21d0fb6b400ab55737f2188df80097cc1cc4ef3e
Idar Borlaug <idar.borlaug@gmail.com>
2019-05-23 16:21:51 +0200

    laster ned validator

--- runSchemaTests.sh
+++ runSchemaTests.sh
@@ -2,6 +2,14 @@
 
 cd "$(dirname "$0")"
 mkdir -p validator
-#wget 
 
+FILE=validator/validator.jar
+
+if [ -f "$FILE" ] 
+then
+	echo "Validator exists, skipping download"
+else
+
+	wget "https://oss.sonatype.org/service/local/artifact/maven/redirect?r=snapshots&g=no.ks.fiks.io.melding&a=fiks-io-melding-validator&v=1.0.0-SNAPSHOT&e=jar" -O $FILE
+fi
 java -jar validator/*.jar  $(pwd)
